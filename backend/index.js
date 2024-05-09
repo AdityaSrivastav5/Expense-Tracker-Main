98ed1fd3a1fa0fd63763ce83679788f99dbb7f3c
adityasrivastav5 <adityasrivastav863@gmail.com>
2024-05-09 11:33:56 +0530

    Describe the changes you made

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
+// index.js
+
 const express = require('express');
 const cors = require('cors');
 const { db } = require('./db/db');
 const { readdirSync } = require('fs');
 const app = express();
-
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3004;
@@ -11,18 +12,25 @@ const PORT = process.env.PORT || 3004;
 app.use(cors());
 app.use(express.json());
 
-// Routes setup
-const routesPath = './routes'; // Define the path to your routes directory
-
-// Dynamically import routes
-readdirSync(routesPath).map(route => app.use('/api/v1/', require(`${routesPath}/${route}`)));
+// Dynamic route setup
+const routesPath = './routes';
+readdirSync(routesPath).forEach((file) => {
+    if (file.endsWith('.js')) {
+        const route = require(`${routesPath}/${file}`);
+        app.use('/api/v1', route);
+    }
+});
 
 // MongoDB connection
-const server = () => {
-    db();
-    app.listen(PORT, () => {
-        console.log('Listening to port:', PORT);
-    });
+const server = async () => {
+    try {
+        await db();
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+    }
 };
 
 server();
